40a6707d726f871ba74bbee648d209d795fcbd23
rajaaghanem <rajaaghanem@gmail.com>
2021-12-20 15:27:45 +0200

    youtube api

---
+++ code-along/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyAjC8kfARnMAplj_dNStc7-iHVanbRdCsA";
+
+export default axios.create({
+ baseURL= 'https://www.googleapis.com/youtube/v3',
+ params:{
+     part: "snippet",
+     maxResults: 5,
+     key: KEY
+ }
+});
