934c8f15f2fa13c972532c8cdacbab8b473a9bb0
rajaaghanem <rajaaghanem@gmail.com>
2021-12-21 19:29:29 +0200

    contorl the input

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,15 +1,28 @@
+import React from "react";
 import "./App.css";
 
 if (module.hot) {
   module.hot.accept();
 }
 
-function App() {
+class App extends React.Component {
+  state = {
+    userInput: "",
+  };
 
-  return (<div className="app-container">
-    <input type="textarea" className="input-style"/>
-    <button className="add-task-style">Add Task</button>
-  </div>);
+  handleInput=(event)=>{
+    this.setState({userInput : event.target.value})
+  }
+
+  render() {
+    console.log(this.state.userInput);
+    return (
+      <div className="app-container">
+        <input type="textarea" className="input-style" onChange={this.handleInput} value={this.state.userInput}/>
+        <button className="add-task-style">Add Task</button>
+      </div>
+    );
+  }
 }
 
 export default App;
