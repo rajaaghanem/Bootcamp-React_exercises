526dac4e9a4aab1977054403361e4d8b574d8dac
rajaaghanem <rajaaghanem@gmail.com>
2021-12-21 21:30:42 +0200

    ..

--- todo-app/src/components/Task/Task.js
+++ todo-app/src/components/Task/Task.js
@@ -3,22 +3,22 @@ import "./Task.css"
 
 class Task extends React.Component {
 
-  state={ update: " ", openInput:" "}
+  state={ update: " "}
 
   componentDidMount=()=>{
     this.setState({update: this.props.text})
   }
 
   handleNewInput=(event)=>{
-    console.log(event.target.value);
     this.setState({update: event.target.value});
   }
 
   handleClickEdit=()=>{
-
+    this.props.update(this.state.update,this.props.id);
   }
 
   render() {
+    console.log(this.props);
     return <div className="task-container">
       <textarea rows="12" cols="28" value={this.state.update} onChange={this.handleNewInput}/>
       <button className="btn-style" onClick={this.handleClickEdit}>Update</button>
