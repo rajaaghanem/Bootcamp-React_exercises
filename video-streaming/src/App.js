70c138ef65e95a2f401a614c1a83837734eb98d0
rajaaghanem <rajaaghanem@gmail.com>
2021-12-21 12:20:57 +0200

    add header

--- video-streaming/src/App.js
+++ video-streaming/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter, Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamCreate from "./components/Streams/StreamCreate";
 import StreamDelete from "./components/Streams/StreamDelete";
 import StreamEdit from "./components/Streams/StreamEdit";
 import StreamList from "./components/Streams/StreamList";
 import StreamShow from "./components/Streams/StreamShow";
+import Header from "./components/Streams/Header/Header";
 
 const App =()=>{
 
     return (
-      <div>
+      <div className="ui container">
+       
         <BrowserRouter>
             <div>
+              <Header/>
               <Route path="/" exact component={StreamList}/>
               <Route path="/streams/new" exact component={StreamCreate}/>
               <Route path="/streams/edit" exact component={StreamEdit}/>
