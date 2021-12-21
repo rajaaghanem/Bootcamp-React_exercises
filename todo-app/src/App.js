3067c7ff57589ba6135e5af0dd6d69ad297a48e8
rajaaghanem <rajaaghanem@gmail.com>
2021-12-21 19:43:15 +0200

    add uniqe id

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Task from "./components/Task/Task";
+import { v4 as uuidv4 } from 'uuid';
 
 if (module.hot) {
   module.hot.accept();
@@ -8,18 +10,25 @@ if (module.hot) {
 class App extends React.Component {
   state = {
     userInput: "",
+    tasks:[],
   };
 
   handleInput=(event)=>{
     this.setState({userInput : event.target.value})
   }
 
+  createTask=()=>{
+    const id = uuidv4();
+    const task = <Task key={id}/>;
+    this.setState({tasks:[...this.state.tasks, task]})
+  }
+
   render() {
-    console.log(this.state.userInput);
     return (
       <div className="app-container">
         <input type="textarea" className="input-style" onChange={this.handleInput} value={this.state.userInput}/>
-        <button className="add-task-style">Add Task</button>
+        <button className="add-task-style" onClick={this.createTask}>Add Task</button>
+        {this.state.tasks}
       </div>
     );
   }
---
+++ todo-app/src/components/Task/Task.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Task extends React.Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default Task;
