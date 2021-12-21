898dc4d40d3bec00d9f1d44d6ee781adeb564aa7
rajaaghanem <rajaaghanem@gmail.com>
2021-12-21 14:03:30 +0200

    add store

--- 16.1-products/src/components/Products/Products.js
+++ 16.1-products/src/components/Products/Products.js
@@ -1,11 +1,18 @@
 import React from "react";
 // import{Link} from 'react-router-dom';
+import store from "../../store/store";
 
-const Products=()=>{
-    return(
-        <div className="">
-            products
-        </div>
-    );
+class Products extends React.Component {
+  state = { data: [] };
+
+  componentDidMount = () => {
+    this.setState({ data: store });
+  };
+
+  render() {
+    console.log(this.state.data);
+    return <div className="">products</div>;
+  }
 }
+
 export default Products;
