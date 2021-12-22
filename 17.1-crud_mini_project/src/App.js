027bca6c8a55e8d4f05e0b3e60b257e8b217df09
rajaaghanem <rajaaghanem@gmail.com>
2021-12-22 14:41:21 +0200

    handle delete

--- 17.1-crud_mini_project/src/App.js
+++ 17.1-crud_mini_project/src/App.js
@@ -2,22 +2,35 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 import Student from "./components/Student/Student";
+import { v4 as uuidv4 } from "uuid";
 
 class App extends React.Component {
   state = { students: [], userInputName: "", userInputImg: " " };
 
-  componentDidMount=async ()=>{
-    const response = await axios.get("https://61c300cc9cfb8f0017a3e86b.mockapi.io/users");
-    this.setState({students: response.data})
-    console.log("api",response);
-  }
+  componentDidMount = async () => {
+    const response = await axios.get(
+      "https://61c300cc9cfb8f0017a3e86b.mockapi.io/users"
+    );
+    this.setState({ students: response.data });
+    console.log("api", response);
+  };
 
-  displayStudent=()=>{
-    return this.state.students.map((student)=>{
-      return <Student key={student.id} id={student.id} name={student.name} imgURL={student.imgURL} update={this.handleUpdate}/>
-    })
-  }
- 
+  displayStudent = () => {
+    return this.state.students.map((student) => {
+      return (
+        <Student
+          key={student.id}
+          id={student.id}
+          name={student.name}
+          imgURL={student.imgURL}
+          update={this.handleUpdate}
+          delete={this.handleDelete}
+        />
+      );
+    });
+  };
+
+  //update the user input in state
   handleUpdate = (textupdate, id) => {
     const students = [...this.state.students];
     const student = students.find((student) => student.id === id);
@@ -32,6 +45,7 @@ class App extends React.Component {
     });
   };
 
+  //update the state depends on the user inputs
   handleChange = (event) => {
     const inputType = event.target.name;
     switch (inputType) {
@@ -43,16 +57,60 @@ class App extends React.Component {
     }
   };
 
+  //add new student to the api and to the state
+  handleCreate = async () => {
+    const student = {
+      // id : uuidv4(),
+      name: this.state.userInputName,
+      imgURL: this.state.imgURL,
+    };
+    try {
+      const response = await axios.post(
+        `https://61c300cc9cfb8f0017a3e86b.mockapi.io/users`,
+        student
+      );
+      console.log("api add", response);
+      this.setState({
+        students: [...this.state.students, student],
+      });
+    } catch (e) {}
+  };
+
+  handleDelete = async (id) => {
+    try{
+      const response = await axios.delete(
+        `https://61c300cc9cfb8f0017a3e86b.mockapi.io/users/${id}`
+      );
+      console.log("delete api", response);
+      const filteredList = this.state.students.filter((student) => {
+        return student.id !== id;
+      });
+  
+      this.setState({ students: filteredList });
+    }catch(e){}
+    
+  };
+
   render() {
-    console.log("students state",this.state.students);
+    console.log("students state", this.state.students);
 
     return (
       <>
         <label>Name:</label>
-        <input type="text" name="inputName" onChange={this.handleChange} value={this.state.userInputName}/>
+        <input
+          type="text"
+          name="inputName"
+          onChange={this.handleChange}
+          value={this.state.userInputName}
+        />
         <label>Img Url:</label>
-        <input type="text" name="inputImg" onChange={this.handleChange} value={this.state.userInputImg}/>
-        <button>Add Student</button>
+        <input
+          type="text"
+          name="inputImg"
+          onChange={this.handleChange}
+          value={this.state.userInputImg}
+        />
+        <button onClick={this.handleCreate}>Add Student</button>
         <div className="students-container"> {this.displayStudent()}</div>
       </>
     );
--- 17.1-crud_mini_project/src/components/Student/Student.js
+++ 17.1-crud_mini_project/src/components/Student/Student.js
@@ -3,36 +3,46 @@ import axios from "axios";
 import "./Student.css";
 
 class Student extends React.Component {
-    state={update:""};
+  state = { update: "" };
 
-    componentDidMount=()=>{
-        this.setState({update: this.props.name})
-      }
+  componentDidMount = () => {
+    this.setState({ update: this.props.name });
+  };
 
-    handleNewInput=(event)=>{
-        this.setState({update: event.target.value});
-      }
+  handleNewInput = (event) => {
+    this.setState({ update: event.target.value });
+  };
 
-      handleClickEdit=async()=>{
-        const student = {
-            id:this.props.id,
-            name: this.state.update,
-            imgURL: this.props.imgURL,
-        }
-        const response = await axios.put(`https://61c300cc9cfb8f0017a3e86b.mockapi.io/users/${this.props.id}`,student);
-        console.log("api update:", response);
-        this.props.update(this.state.update,this.props.id);
-      }
+  handleClickEdit = async () => {
+    const student = {
+      id: this.props.id,
+      name: this.state.update,
+      imgURL: this.props.imgURL,
+    };
+    try{
+        const response = await axios.put(
+            `https://61c300cc9cfb8f0017a3e86b.mockapi.io/users/${this.props.id}`,
+            student
+          );
+          console.log("api update:", response);
+          this.props.update(this.state.update, this.props.id);
+    }catch(e){}
+    
+  };
 
-    render(){
-        return(
-            <div className="student-box">
-                <input value={this.state.update} onChange={this.handleNewInput}/>
-                <img src={`${this.props.imgURL}`} className="img-box"/>
-                <button onClick={this.handleClickEdit}>Update</button>
-                <button>Delete</button>
-            </div>
-        );
-    }
+  handleClickDelete=()=>{
+    this.props.delete(this.props.id);
+  }
+
+  render() {
+    return (
+      <div className="student-box">
+        <input value={this.state.update} onChange={this.handleNewInput} />
+        <img src={`${this.props.imgURL}`} className="img-box" />
+        <button onClick={this.handleClickEdit}>Update</button>
+        <button onClick={this.handleClickDelete}>Delete</button>
+      </div>
+    );
+  }
 }
 export default Student;
