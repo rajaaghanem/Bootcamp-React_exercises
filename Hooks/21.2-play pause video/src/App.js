e3c174109c4e9ec520153cb576d2c97502e94fa0
rajaaghanem <rajaaghanem@gmail.com>
2021-12-28 15:55:11 +0200

    done

--- Hooks/21.2-play pause video/src/App.js
+++ Hooks/21.2-play pause video/src/App.js
@@ -5,21 +5,25 @@ const App = () => {
   const myVid = useRef("");
 
   const handlePlay = () => {
-    myVid.current.play();
+    myVid.play();
   };
 
   const handlePause = () => {
-    myVid.current.pause();
+    myVid.pause();
   };
 
   return (
     <div>
-      <video ref={myVid} width="750" height="500" controls>
-        <source
-          src="myvideo.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <iframe
+        ref={myVid}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/fUXdrl9ch_Q"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
       <button onClick={handlePlay}>Play</button>
       <button onClick={handlePause}>Pause</button>
     </div>
